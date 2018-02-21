9dce86052494a9755413a5c593b5b12942272e5e
rbalykov <rbalykov@yandex.ru>
2018-02-21 09:59:54 +0300

    Рабочее демо (эхо в терминале)

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -292,6 +292,10 @@ static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
+  
+// гюлемхрэ щун мю врн-рн нялшякеммне
+  CDC_Transmit_FS(Buf, *Len);
+  
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
   /* USER CODE END 6 */
