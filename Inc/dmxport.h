6cc9e3cfcfd12bc3eb202144f36445158705e150
rbalykov <rbalykov@yandex.ru>
2018-09-16 00:48:55 +0300

    add c++ code

--- Inc/dmxport.h
+++ Inc/dmxport.h
@@ -12,23 +12,42 @@ namespace dmx
 
 		class result_t {};
 		class direction_t {};
+
 		
-		class port
+		namespace port
 		{
-			public:
-				result_t setDirection (direction_t d);
-				result_t sendData (const dataframe_t&);
-				dataframe_t receivedData ();
-				
-				void hookUart (UART_HandleTypeDef*);
-			private:
-				direction_t dataFlowDirection;
-				UART_HandleTypeDef * uartHal;
+			namespace tx
+			{
+				result_t dataHandler (dataframe_t&);
+				result_t arm (void);
+				result_t disarm (void);
+				result_t run (void);
+				result_t stop (void);
+			}
+			namespace rx
+			{
+				result_t dataHandler (dataframe_t&);
+				result_t arm (void);
+				result_t disarm (void);
+				result_t run (void);
+				result_t stop (void);
+			}
+			class control_t
+			{
+				public:
+					result_t setDirection (direction_t d);
+					result_t sendData (const dataframe_t&);
+					dataframe_t receivedData ();
+					
+					void hookUart (UART_HandleTypeDef*);
+				private:
+					direction_t dataFlowDirection;
+					UART_HandleTypeDef * uartHal;
+			};
 		};
 
-
-	} //io
-} // dmx
+	}; //io
+}; // dmx
 
 
  
